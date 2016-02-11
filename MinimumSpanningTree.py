c3950eebeaeb02e1d000a3f8756816ea82ca5f2e
magneton_bora <dryndikroman@gmail.com>
2016-02-11 22:33:25 +0100

    Kruskal's algorithm

--- MinimumSpanningTree.py
+++ MinimumSpanningTree.py
@@ -1,7 +1,29 @@
+import unittest
 from ClassWeightedGraph import *
+from UnionFind import UnionFind
+
+def MinimumSpanningTree(G):
+    # Kruskal's algorithm: sort edges by weight, and add them one at a time.
+    # We use Kruskal's algorithm, first because it is very simple to
+    # implement once UnionFind exists, and second, because the only slow
+    # part (the sort) is sped up by being built in to Python.
+    subtrees = UnionFind()
+    tree = []
+    for W,u,v in sorted((G[u][v],u,v) for u in G for v in G[u]):
+        if subtrees[u] != subtrees[v]:
+            tree.append((u,v))
+            subtrees.union(u,v)
+    return tree
+
 
 def GetMinimumSpanningTree(graph):
+    subtrees = UnionFind()
     spanningTree = WeightedGraph()
-    #TODO
+    sorted_weighted_edges_list = sorted(
+        (graph[u][v], u, v) for u in graph for v in graph[u]
+    )
+    for W, u, v in sorted_weighted_edges_list:
+        if subtrees[u] != subtrees[v]:
+            spanningTree.AddEdge(u, v, W)
+            subtrees.union(u, v)
     return spanningTree
-
--- TestMinimumSpanningTree.py
+++ TestMinimumSpanningTree.py
@@ -2,6 +2,9 @@ import unittest
 from ClassWeightedGraph import *
 from MinimumSpanningTree import *
 
+# https://www.ics.uci.edu/~eppstein/PADS/MinimumSpanningTree.py
+
+
 class TestSpanningTree(unittest.TestCase):
     
     def setUp(self):
@@ -11,17 +14,28 @@ class TestSpanningTree(unittest.TestCase):
 
 
     def test_something(self):
-        #TODO: test something
+        G = {
+                0: {1:11, 2:13, 3:12},
+                1: {0:11, 3:14},
+                2: {0:13, 3:10},
+                3: {0:12, 1:14, 2:10}
+            }
+        T = [(2, 3), (0, 1), (0, 3)]
+
+        minimum_spaning_tree = GetMinimumSpanningTree(G)
+
+        print(minimum_spaning_tree)
+        for e, f in zip(minimum_spaning_tree, T):
+            print(e, f)
+
+        self.assertEqual(
+            minimum_spaning_tree,
+            {0: {1: 11, 3: 12}, 1: {0: 11}, 2: {3: 10}, 3: {0: 12, 2: 10}}
+        )
 
     def test_something_else(self):
-        #TODO: test something else
-
-        #...
+        pass
 
 
-if __name__ == '__main__':                      
+if __name__ == '__main__':
     unittest.main()
-
-
-
-                      
